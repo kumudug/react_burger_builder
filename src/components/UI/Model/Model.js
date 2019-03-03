374bf1e8c48c4ebc5af7ead94f805c6416e54142
kumudug <kumudug1@gmail.com>
2019-03-03 11:42:42 -0800

    Resolves #12. Add a backdrop to the Model

--- src/components/UI/Model/Model.js
+++ src/components/UI/Model/Model.js
@@ -1,18 +1,22 @@
 import React from 'react';
 
 import styles from './Model.module.css';
+import Backdrop from '../Backdrop/Backdrop';
 
 const model = (props) => (
-    <div
-        className={styles.Modal}
-        style={
-            {
-                transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-                opacity: props.show ? '1' : '0'
-            }
-        }>
-        {props.children}
-    </div>
+    <React.Fragment>
+        <Backdrop show={props.show} click={props.cancel} />
+        <div
+            className={styles.Modal}
+            style={
+                {
+                    transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
+                    opacity: props.show ? '1' : '0'
+                }
+            }>
+            {props.children}
+        </div>
+    </React.Fragment>
 );
 
 export default model;
