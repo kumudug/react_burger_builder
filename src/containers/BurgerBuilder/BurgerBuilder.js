6d98c3b3fb9dee59de4ebdf683b01f0907894196
kumudug <kumudug1@gmail.com>
2019-03-02 17:31:43 -0800

    Fix #1 issue: 'missing BurgerBuilder import'

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import Burger from '../../components/Burger/Burger';
+import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
 class BurgerBuilder extends Component {
 
