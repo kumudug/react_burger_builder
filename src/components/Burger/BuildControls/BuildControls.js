89cb331c3e3ba1311a274a39774e420b2a555aef
kumudug <kumudug1@gmail.com>
2019-03-02 19:42:24 -0800

    #3 Dynamically add ingredients to burger

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -4,16 +4,19 @@ import styles from './BuildControls.module.css';
 import BuildControl from './BuildControl/BuildControl';
 
 const controls = [
-    { label: 'Salad', type:'salad'},
-    { label: 'Bacon', type:'bacon'},
-    { label: 'Cheese', type:'cheese'},
-    { label: 'Meat', type:'meat'},
+    { label: 'Salad', type: 'salad' },
+    { label: 'Bacon', type: 'bacon' },
+    { label: 'Cheese', type: 'cheese' },
+    { label: 'Meat', type: 'meat' },
 ];
 
 const buildControls = (props) => (
     <div className={styles.BuildControls}>
         {controls.map(ctrl => (
-            <BuildControl key={ctrl.label} label={ctrl.label}/>
+            <BuildControl
+                key={ctrl.label}
+                label={ctrl.label}
+                added={() => props.ingredientAdded(ctrl.type)} />
         ))}
     </div>
 );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,22 +3,49 @@ import React, { Component } from 'react';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 
+const INGREDIENT_PRICES = {
+    salad: 0.5,
+    bacon: 0.7,
+    cheese: 0.4,
+    meat: 1.3
+}
+
 class BurgerBuilder extends Component {
 
     state = {
         ingredients: {
-            salad: 1,
-            bacon: 1,
-            cheese: 2,
-            meat: 2
-        }
+            salad: 0,
+            bacon: 0,
+            cheese: 0,
+            meat: 0
+        },
+        totalPrice: 4
     };
+
+    addIngredientHandler = (type) => {
+        const updatedIngredients = {
+            ...this.state.ingredients
+        };
+        updatedIngredients[type] = this.state.ingredients[type] + 1;
+        const updatedPrice = this.state.totalPrice + INGREDIENT_PRICES[type];
+        this.setState({
+            ingredients: updatedIngredients,
+            totalPrice: updatedPrice
+        });
+    };
+
+    removeIngredientHandler = (type) => {
+
+    };
+
+    remo
 
     render() {
         return (
             <React.Fragment>
-                <Burger ingredients={this.state.ingredients}/>
-                <BuildControls />
+                <Burger ingredients={this.state.ingredients} />
+                <BuildControls
+                    ingredientAdded={this.addIngredientHandler} />
             </React.Fragment>
         );
     }
