42f7031dd22fa1dacf7d9d5f6317cb7996d050ff
kumudug <kumudug1@gmail.com>
2019-03-03 18:44:13 -0800

    Resolves #16. Add toolbar to burger builder

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 import styles from "./Layout.module.css";
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 
 const layout = (props) => (
     <React.Fragment>
-        <div>Toolbar, SideDrawer, Backdrop</div>
+        <Toolbar />
         <main className={styles.Content}>
             {props.children}
         </main>
