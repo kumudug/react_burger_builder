67b48dbaeee3543949d131f155b00de14f73f7f8
kumudug <kumudug1@gmail.com>
2019-03-17 12:32:15 -0700

    Fixes 34. Model changes when child state changes

--- src/components/UI/Model/Model.js
+++ src/components/UI/Model/Model.js
@@ -6,9 +6,9 @@ import Backdrop from '../Backdrop/Backdrop';
 class Model extends Component {
 
     shouldComponentUpdate(nextProps, nextState) {
-        return nextProps.show != this.props.show;
+        return nextProps.show != this.props.show || nextProps.children !== this.props.children;
     }
-
+ 
     render() {
         return (
             <React.Fragment>
