e5f8d2aed91e6018ed39137a555b16c485fe644b
kumudug <kumudug1@gmail.com>
2019-03-17 17:47:13 -0700

    Fixes #39. Cleanup of withErrorHandler

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -4,16 +4,17 @@ import Model from '../../components/UI/Model/Model';
 
 const withErrorHandler = (WrappedComponent, axios) => {
     return class extends Component {
+
         constructor(props) {
             super(props);
             this.state = {
                 error: null
             };
-            axios.interceptors.request.use(req => {
+            this.reqInterceptor = axios.interceptors.request.use(req => {
                 this.setState({ error: null });
                 return req;
             });
-            axios.interceptors.response.use(res => res, error => {
+            this.resInterceptor = axios.interceptors.response.use(res => res, error => {
                 console.log(error);
                 this.setState({ error: error });
             });
@@ -21,6 +22,11 @@ const withErrorHandler = (WrappedComponent, axios) => {
 
         errorConfirmedHandler = () => {
             this.setState({ error: null });
+        }
+
+        componentWillUnmount() {
+            axios.interceptors.request.eject(this.reqInterceptor);
+            axios.interceptors.response.eject(this.resInterceptor);
         }
 
         render() {
